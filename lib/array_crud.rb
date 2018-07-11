36f35a4aac22696c715525258af2d3d6ae56a493
angelica winkler <caw400@nyu.edu>
2018-07-11 00:12:27 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   array = ["live", "laugh", "love", "fart"]
 end
 
-def add_element_to_end_of_array(array, element)
-
+def add_element_to_end_of_array(someArray, someElement)
+  some
 end
 
 def add_element_to_start_of_array(array, element)
