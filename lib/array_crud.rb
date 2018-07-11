455149e3abeaa0a548af3b75e283302779a1e29c
angelica winkler <caw400@nyu.edu>
2018-07-11 00:16:05 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,8 +10,8 @@ def add_element_to_end_of_array(someArray, someElement)
     someArray << someElement
 end
 
-def add_element_to_start_of_array(array, element)
-  array.unshift("element")
+def add_element_to_start_of_array(someArray, someElement)
+  someArray.unshift("someElement")
 end
 
 def remove_element_from_end_of_array(array)
